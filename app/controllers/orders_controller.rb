269e8d05ae86f2fd29fbd6696627759368b6453d
zendex25 <zendex25@yahoo.com>
2014-04-14 14:42:11 +0800

    Fixed minor bug with Order Controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -41,7 +41,7 @@ class OrdersController < ApplicationController
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
         OrderNotifier.received(@order).deliver
-        format.html { redirect_to shop_store_url, notice: 'Thank you for your order.' }
+        format.html { redirect_to store_url, notice: 'Thank you for your order.' }
         format.json { render action: 'show', status: :created, location: @order }
       else
         format.html { render action: 'new' }
