28767554f8bcab353ea7b8aed7de9421732ed126
zendex25 <zendex25@yahoo.com>
2014-04-25 19:47:46 +0800

    Minor fixes

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,7 +5,7 @@ class Order < ActiveRecord::Base
 	PAYMENT_TYPES = ["Purchase Order"]
 
 	validates :name, :address, :email, :contact, presence: true
-	validates :contact, :numericality => true
+	validates :contact, format: { with: /\d{3}-\d{3}-\d{4}/ }
 	validates :pay_type, inclusion: PAYMENT_TYPES
 
 	def add_line_items_from_cart(cart)
--- app/models/support.rb
+++ app/models/support.rb
@@ -3,6 +3,7 @@ class Support < ActiveRecord::Base
 
 
 validates :name, :title, :body, :email, :contact, presence: true
+validates :contact, format: { with: /\d{4}-\d{3}-\d{4}/ }
 
 def index
 	@fronts = Front.all
