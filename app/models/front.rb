01c08afc92cd683beb5d1ac645a4e651aee5df1a
zendex25 <zendex25@yahoo.com>
2014-04-30 11:40:08 +0800

    wtf

--- app/models/front.rb
+++ app/models/front.rb
@@ -1,6 +1,6 @@
 class Front < ActiveRecord::Base
 
-	validates :img1, :img2, :img3, :about, :company, :services, :dealer, :img4, :img5, :img6, :img7, :img8, :img9, :img10, :faq, :shop, :how, :contact, :store_loc, presence: true
+	validates :img1, :img2, :img3, :about, :company, :services, :bottom, :dealer, :img4, :img5, :img6, :img7, :img8, :img9, :img10, :faq, :shop, :how, :contact, :store_loc, presence: true
 
 	validates :img1, :img2, :img3, :img4, :img5, :img6, :img7, :img8, :img9, :img10, format: {
 with: %r{\.(gif|jpg|png)\Z}i,
